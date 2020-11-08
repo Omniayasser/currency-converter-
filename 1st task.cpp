8680463495a6cd88c14e4559762713546262fc25
Omniayasser <73958351+Omniayasser@users.noreply.github.com>
2020-11-08 19:56:40 +0200

    Create 1st task.cpp

---
+++ 1st task.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	float currency; 
+	printf(" CURRENCY CONVERTER FROM USD TO EGP\n"); 
+	printf("plese, enter your currency."); 
+	scanf("%f", & currency); 
+	currency = currency + 15.69;
+	printf("%.2f", currency);
+}
